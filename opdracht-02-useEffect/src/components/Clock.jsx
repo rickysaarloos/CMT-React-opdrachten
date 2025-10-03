292d116082d667d8c2e5b20ef204db0395be360d
rickysaarloos <9027376@student.zadkine.nl>
2025-10-03 04:49:26 +0200

    Opdracht 2: Clock

--- opdracht-02-useEffect/src/components/Clock.jsx
+++ opdracht-02-useEffect/src/components/Clock.jsx
@@ -2,11 +2,11 @@ import { useState, useEffect } from "react";
 
 const Clock = () => {
 
-    const [time, setTime] = useState(new Date().toLocaleTimeString());
+    const [currentTime , setCurrentTime] = useState(new Date().toLocaleTimeString());
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            setTime(new Date().toLocaleTimeString());
+            setCurrentTime(new Date().toLocaleTimeString());
             console.log('tick');
         }, 1000);
         return () => {
@@ -17,9 +17,10 @@ const Clock = () => {
     return (
         <div>
             <h1>Clock</h1>
-            <p>{time}</p>
+            <p>{currentTime }</p>
         </div>
     );
 }
 
 export default Clock;
+
